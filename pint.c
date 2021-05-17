e2c2cfa3bc68603ea81015b6e3e4fdad45d91434
KennyReyesS <kennyreyes2000@gmail.com>
2021-05-17 02:42:09 +0000

    function created

---
+++ pint.c
@@ -0,0 +1,21 @@
+#include "monty.h"
+
+/**
+ * pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: pointer to the head of the stack
+ * @line_number: The line number
+ */
+
+void pint(stack_t **stack, unsigned int line_number)
+{
+	stack_t *h = *stack;
+
+	if (h == NULL)
+	{
+		dprintf(STDERR_FILENO, "L%u: can't pint, stack empty\n", line_number);
+		cleanmemory();
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", h->n);
+}
